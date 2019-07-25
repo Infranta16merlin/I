4edececf71a9d30d9f6232f7720a14559fd7c8e9
Infranta16merlin <46000315+Infranta16merlin@users.noreply.github.com>
2019-07-25 16:38:10 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,6 +1,6 @@
-n=input()
-if(n.isalpha()==True):
-    if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
+c=input()
+if(c.isalpha()==True):
+    if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u'):
         print("vowels")
     else:
         print("consonants")
