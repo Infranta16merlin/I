8f3c784a186ad6159b93c5438169fcdbb23e2a42
Infranta16merlin <46000315+Infranta16merlin@users.noreply.github.com>
2019-07-25 03:59:54 -0700

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,8 @@
+n=input()
+if(n.isalpha()==True):
+    if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
+        print("vowels")
+    else:
+        print("consonants")
+else:
+    print("invalid")
